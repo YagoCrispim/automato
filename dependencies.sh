72dc237f6d27bcac339272b7ca67fe1943ac8323
Yago Crispim <34103098+YagoCrispim@users.noreply.github.com>
2021-05-14 22:46:53 -0300

    Dependencies installer

---
+++ dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Dependencies installer
+
+echo ""
+echo "-------------------------"
+echo "Dependencies installer"
+echo "-------------------------"
+echo ""
+
+sudo apt update
+sudo apt install -y zenity rsync
